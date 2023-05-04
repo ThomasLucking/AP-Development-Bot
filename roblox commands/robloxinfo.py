28856d88376c3563d3fadb3e0a49ccc0c8971aa4
first app <telucking@gmail.com>
2023-05-04 20:15:54 +0200

    improving code

--- roblox commands/robloxinfo.py
+++ roblox commands/robloxinfo.py
@@ -6,6 +6,7 @@ import datetime
 from discord.ext import commands
 from discord import app_commands
 
+
 class userinfo(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -22,65 +23,72 @@ class userinfo(commands.Cog):
             file_path = "./data/roblox.json"
             with open(file_path, "r") as f:
                 roblox_data = json.load(f)
+            
+            a = json.dumps(roblox_data)
+            x = json.loads(a)
+
+            b = x["endpoints"]["user_info"]
+            user_info = requests.get(b.format(user_id=user_id)).json()
+            print(user_info.keys())
+            print(user_info.values())
+            i = []
+            for x in user_info.values():
+                i.append(x)
+            #print(i.keys())
+            #date = i[1][0:10]
+            #i[1] = date
+            date = user_info["created"][0:10]
+            user_info["created"] = date
+
             print("Roblox data loaded.")
         except Exception as e:
             print(f"Error loading Roblox data: {e}")
-
-        endpoints = roblox_data["endpoints"]
-        fields = roblox_data["fields"]
-
-        # get user data by id
-        user_info = requests.get(endpoints["user_info"].format(user_id=user_id)).json()
-        if "id" not in user_info:
-            return await interaction.response.send_message("User not found")
-
-        # get user display name by id
-        display_info = requests.get(endpoints["display_info"].format(user_id=user_id)).json()
-        display_name = display_info[fields["display_name"][0]]
-
-        # get user account creation date by id
-        created_info = requests.get(endpoints["created_info"].format(user_id=user_id)).json()
-        account_created = created_info[fields["account_created"][0]]
-
-        # get user description by id
-        desc_info = requests.get(endpoints["desc_info"].format(user_id=user_id)).json()
-        user_desc = desc_info.get(fields["user_desc"][0], fields["user_desc"][1])
-                
+       
         # get user profile image by id
-        profile_info = requests.get(endpoints["profile_info"].format(user_id=user_id)).json()
-        profile_image = profile_info[fields["profile_image"][0]] [fields["profile_image"][1]][fields["profile_image"][2]]
+        endpoints = roblox_data["endpoints"]
+        profile_info = requests.get(endpoints["profile_info"].format(user_id=i[5])).json()
+        profile_image = (profile_info["data"][0]["imageUrl"])
+        #profile_image = profile_info[fields["profile_image"][0]] [fields["profile_image"][1]][fields["profile_image"][2]]
 
-        embed = discord.Embed(title=f"{display_name}", url=f"https://www.roblox.com/users/{user_id}/profile", color=0x00b3ff)
+        #embed = discord.Embed(title=f"{i[7]}", url=f"https://www.roblox.com/users/{i[5]}/profile", color=0x00b3ff)
+        embed = discord.Embed(title=f"{i[7]}", url=f"https://www.roblox.com/users/{i[5]}/profile", color=0x00b3ff)
         embed.set_author(name="Roblox user")
         embed.set_footer(text=f"Made by {interaction.user}")
-        embed.add_field(name="Id", value=f"{user_id}", inline=False)
-        embed.add_field(name="Display name", value=f"{display_name}", inline=True)
-        embed.add_field(name="Account created", value=f"{account_created}", inline=False)
-        embed.add_field(name="Description", value=f"{user_desc}", inline=True)
+
+        #order = [ ["name", 6],["Display name", 7], ["id", 5],["Account created",1],["Description", 0] ]
+        #for attr in order:
+          #  embed.add_field(name=attr[0], value=f"{i[attr[1]]}", inline=False)
+
+        #for key, value in user_info.items():
+            #print(key, value)
+            #embed.add_field(name=f"{key}", value=f"{value}", inline=False)
+
+        #order = ["name","displayName", "id", "created","description"]
+        #title = ["Name","Display Name", "Id", "Account Created ","Description"]
+        #for attr in order:
+            #print(user_info[attr])
+            #embed.add_field(name=f"{attr}", value=f"{user_info[attr]}", inline=False)
+
+        #for attr in range(0, len(order)):
+            #embed.add_field(name=f"{title[attr]}", value=f"{user_info[order[attr]]}", inline=False)
+
+        order = [["name", "Name"], ["displayName", "Display Name"],["id","Id" ], ["created","Account Created" ], ["description", "Description"]]
+        for attr in order:
+            #embed.add_field(name=f"{attr[0]}", value=f"{user_info[attr[1]]}", inline=False)
+            #{user_info[order[attr]]}
+            sattr = attr
+            embed.add_field(name=f"{sattr[1]}", value=f"{user_info[sattr[0]]}", inline=False)
+
+        #embed.add_field(name="Id", value=f"{i[5]}", inline=False)
+        #embed.add_field(name="Display name", value=f"{i[7]}", inline=False)
+        #embed.add_field(name="Account created", value=f"{i[1]}", inline=False)
+        #embed.add_field(name="Description", value=f"{i[0]}", inline=False)
+
         embed.set_thumbnail(url=f"{profile_image}")
 
 
-
-        # send embed
         await interaction.response.send_message(embed=embed)
         
-
-
-
-
-
-
-
- 
-
-
-
-    
-
-    
-
-
-
 
 async def setup(client):
     await client.add_cog(userinfo(client))
